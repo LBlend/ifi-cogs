2f29b8b45259aec52398db3c9092ce7e73175ce4
LBlend <lblend@protonmail.com>
2021-12-31 10:15:47 +0100

    remove all unecessary cogs

--- latex/__init__.py
+++
@@ -1,6 +0,0 @@
-from .latex import Latex
-
-__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
-
-def setup(bot):
-  bot.add_cog(Latex(bot))
--- voicelogs/__init__.py
+++
@@ -1,6 +0,0 @@
-from .voicelogs import VoiceLogs
-
-
-async def setup(bot):
-    cog = VoiceLogs(bot)
-    bot.add_cog(cog)
